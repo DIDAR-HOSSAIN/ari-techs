15e7c2836b160f746a479fa8ffbc07c9008399e9
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2024-01-04 20:59:25 +0600

    added footer

--- resources/js/ui/Footer.jsx
+++ resources/js/ui/Footer.jsx
@@ -1,61 +1,84 @@
 import React from "react";
-import logo from "@/assets/images/Logo/logo-transparent.png";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { Link } from "@inertiajs/react";
+import logo from "@/assets/images/Logo/logo-transparent.png";
 
 const Footer = () => {
+    const redirectToExternalLink = (url) => {
+        window.open(url, "_blank");
+    };
+
     return (
-        <footer className="footer mt-4 p-10 bg-base-200 text-base-content place-items-center">
-            <div>
-                <Link to={"/"}>
-                    <img src={logo} alt="" width={150} height={150} />
-                </Link>
-                <p>
-                    ARI Techs.
-                    <br />
-                    Providing Reliable tech since 2022
-                </p>
-            </div>
-            <div>
-                <span className="footer-title">Services</span>
-                <Link to="" className="link link-hover">
-                    Branding
-                </Link>
-                <Link to="" className="link link-hover">
-                    Design
-                </Link>
-                <Link to="" className="link link-hover">
-                    Marketing
-                </Link>
-                <Link to="" className="link link-hover">
-                    Advertisement
-                </Link>
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <Link to="/about" className="link link-hover">
-                    About us
-                </Link>
-                <Link to="/contact" className="link link-hover">
-                    Contact
-                </Link>
-                <Link to="" className="link link-hover">
-                    Jobs
-                </Link>
-                <Link to="" className="link link-hover">
-                    Press kit
-                </Link>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <Link to="" className="link link-hover">
-                    Terms of use
-                </Link>
-                <Link to="" className="link link-hover">
-                    Privacy policy
-                </Link>
-                <Link to="" className="link link-hover">
-                    Cookie policy
-                </Link>
+        <footer className="bg-blue-600 text-white py-8 text-center">
+            <div className="container mx-auto px-4 flex flex-col justify-center items-center">
+                {/* Logo */}
+                <div className="mb-8 justify-center">
+                    <img className="h-24" src={logo} alt="" />
+                </div>
+
+                {/* First Row */}
+                <div className="flex justify-center mb-8">
+                    <ul className="text-lg flex flex-wrap gap-6 justify-center">
+                        <li>About Us</li>
+                        <li>Blog</li>
+                        <li>Jobs</li>
+                        <li>Press</li>
+                        <li>Accessibility</li>
+                        <li>Partners</li>
+                    </ul>
+                </div>
+                {/* Add more columns for other sections in the first row */}
+
+                {/* Second Row */}
+                <div className="flex justify-center mb-8">
+                    <div className="flex items-center space-x-4">
+                        <Link
+                            onClick={() =>
+                                redirectToExternalLink(
+                                    "https://www.facebook.com"
+                                )
+                            }
+                        >
+                            <FaFacebook className="text-3xl cursor-pointer" />
+                        </Link>
+                        <Link
+                            onClick={() =>
+                                redirectToExternalLink(
+                                    "https://www.twitter.com"
+                                )
+                            }
+                        >
+                            <FaTwitter className="text-3xl cursor-pointer" />
+                        </Link>
+                        <Link
+                            onClick={() =>
+                                redirectToExternalLink(
+                                    "https://www.instagram.com"
+                                )
+                            }
+                        >
+                            <FaInstagram className="text-3xl cursor-pointer" />
+                        </Link>
+                        <Link
+                            onClick={() =>
+                                redirectToExternalLink(
+                                    "https://www.linkedin.com"
+                                )
+                            }
+                        >
+                            <FaLinkedin className="text-3xl cursor-pointer" />
+                        </Link>
+                    </div>
+                </div>
+                {/* Add more columns for other sections in the second row */}
+
+                {/* Third Row */}
+                <div className="text-sm">
+                    <p>
+                        Developed by : Ari-Techs | ©2024 Created by JT All
+                        rights reserved.
+                    </p>
+                </div>
             </div>
         </footer>
     );
--- resources/js/ui/Navbar.jsx
+++ resources/js/ui/Navbar.jsx
@@ -8,28 +8,26 @@ import logo from "@/assets/images/Logo/logo-transparent.png";
 const Navbar = () => {
     const { menus } = menu;
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    // const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
     const [hoveredItem, setHoveredItem] = useState(null);
+    const [hoveredMobileMenu, setHoveredMobileMenu] = useState(null);
     const [isEndHovered, setIsEndHovered] = useState(false);
     const isLargeScreen = useMediaQuery({ minWidth: 1024 });
 
-    const handleSubmenuHover = () => {
-        setIsSubmenuOpen(true);
-    };
-
-    const handleSubmenuLeave = () => {
-        if (!hoveredItem && !isSubmenuOpen) {
-            setIsSubmenuOpen(false);
-        }
-    };
-
-    const handleMobileMenuHover = () => {
+    const handleMobileMenuOpen = () => {
         setIsMobileMenuOpen(true);
     };
 
-    const handleMobileMenuLeave = () => {
+    const handleMobileMenuClose = () => {
         setIsMobileMenuOpen(false);
-        setIsSubmenuOpen(false);
+    };
+
+    const handleMobileSubmenuHover = (menuId) => {
+        setHoveredMobileMenu(menuId);
+    };
+
+    const handleMobileSubmenuLeave = () => {
+        setHoveredMobileMenu(null);
     };
 
     const handleEndMenuHover = () => {
@@ -41,64 +39,73 @@ const Navbar = () => {
     };
 
     return (
-        <div className="navbar bg-blue-600 font-bold">
+        <div className="navbar bg-blue-600 font-bold z-10 sticky top-0">
             {/* Mobile Menu */}
-            <div className="navbar-start">
+            <div className="navbar-start relative z-10">
                 <div className="dropdown">
                     <div
                         tabIndex={0}
                         role="button"
                         className="btn btn-ghost lg:hidden"
-                        onMouseEnter={handleMobileMenuHover}
-                        onMouseLeave={handleMobileMenuLeave}
+                        onMouseEnter={handleMobileMenuOpen}
+                        onMouseLeave={handleMobileMenuClose}
                     >
                         <Bars3Icon className="h-6 w-6" />
-                    </div>
-                    {isMobileMenuOpen && (
-                    <ul
-                        tabIndex={0}
-                        className={"menu menu-sm dropdown-content z-[1] p-2 shadow rounded-box w-52"}
-                        onMouseEnter={handleMobileMenuHover}
-                        onMouseLeave={handleMobileMenuLeave}
-                    >
-                        {menus.length > 0 &&
-                            menus.map((menu) => (
-                                <li
-                                    className="text-blue-600 font-bold bg-white"
-                                    key={menu.id}
-                                    onMouseEnter={handleSubmenuHover}
-                                    onMouseLeave={handleSubmenuLeave}
-                                >
-                                    <Link to={menu.path}>{menu.title}</Link>
-                                    {menu?.dropdown?.length > 0 &&
-                                        isSubmenuOpen && (
-                                            <ul className="p-2 text-blue-600 font-bold bg-white absolute top-full left-20 z-[1]">
-                                                {menu?.dropdown?.map(
-                                                    (dropdownMenu) => (
-                                                        <li
-                                                            className=""
-                                                            key={
-                                                                dropdownMenu.id
-                                                            }
-                                                        >
-                                                            <Link
-                                                                to={
-                                                                    dropdownMenu.path
-                                                                }
-                                                            >
-                                                                {
-                                                                    dropdownMenu.title
-                                                                }
-                                                            </Link>
-                                                        </li>
-                                                    )
+
+                        {isMobileMenuOpen && (
+                            <ul
+                                className="menu left-0 top-10 dropdown-content z-[1] p-2 shadow rounded-box w-52"
+                                onMouseEnter={handleMobileMenuOpen}
+                                onMouseLeave={handleMobileMenuClose}
+                            >
+                                {menus.length > 0 &&
+                                    menus.map((menu) => (
+                                        <li
+                                            className="text-blue-600 font-bold bg-white relative"
+                                            key={menu.id}
+                                            onMouseEnter={() =>
+                                                handleMobileSubmenuHover(
+                                                    menu.id
+                                                )
+                                            }
+                                            onMouseLeave={
+                                                handleMobileSubmenuLeave
+                                            }
+                                        >
+                                            <Link to={menu.path}>
+                                                {menu.title}
+                                            </Link>
+                                            {menu?.dropdown?.length > 0 &&
+                                                hoveredMobileMenu ===
+                                                    menu.id && (
+                                                    <ul className="p-2 text-blue-600 font-bold bg-white absolute top-full left-20 z-[1]">
+                                                        {menu?.dropdown?.map(
+                                                            (dropdownMenu) => (
+                                                                <li
+                                                                    className=""
+                                                                    key={
+                                                                        dropdownMenu.id
+                                                                    }
+                                                                >
+                                                                    <Link
+                                                                        to={
+                                                                            dropdownMenu.path
+                                                                        }
+                                                                    >
+                                                                        {
+                                                                            dropdownMenu.title
+                                                                        }
+                                                                    </Link>
+                                                                </li>
+                                                            )
+                                                        )}
+                                                    </ul>
                                                 )}
-                                            </ul>
-                                        )}
-                                </li>
-                            ))}
-                    </ul>
-                    )}
+                                        </li>
+                                    ))}
+                            </ul>
+                        )}
+                    </div>
                 </div>
                 {isLargeScreen && (
                     <Link to={"/"}>
