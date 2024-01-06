b3f8f0a8d0c1e1ce9a26dd1fc6cc775b4c43475b
DIDAR HOSSAIN <didarhossain018@gmail.com>
2024-01-07 00:40:12 +0600

    update carousel

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,10 +1,10 @@
+import Carousel from '@/ui/Carousel';
 import CustomItService from '@/ui/CustomItService';
 import Fold from '@/ui/Fold';
 import Footer from '@/ui/Footer';
 import FreeEvaluation from '@/ui/FreeEvaluation';
 import Navbar from '@/ui/Navbar';
 import OurNetworkService from '@/ui/OurNetworkService';
-import Slider from '@/ui/Slider';
 import WhyChooseUs from '@/ui/WhyChooseUs';
 import YourNetwork from '@/ui/YourNetwork';
 import Contact from '@/ui/contact';
@@ -13,9 +13,9 @@ import React from 'react';
 const Welcome = () => {
 
     return (
-        <div className='lg:mx-20'>
+        <div className='lg:mx-10'>
             <Navbar />
-            <Slider />
+            <Carousel />
             <OurNetworkService />
             <Fold />
             <WhyChooseUs />
