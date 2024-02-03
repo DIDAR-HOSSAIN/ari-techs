2b5274cbff0e2fdb19239cc637e5a77c0e1b7a5f
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2024-02-03 13:52:14 +0600

    added sidebar menu with collapse type menu

---
+++ resources/js/backend/Dashboard/SidebarMenu.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from "@inertiajs/react";
+
+const SidebarMenu = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    return (
+        <div>
+            <div className="flex items-center">
+                <button onClick={toggleDropdown} className="">
+                    {showDropdown ? "Users ▲" : "Users ▼"}
+                </button>
+            </div>
+            {showDropdown && (
+                <div className="flex flex-col items-center">
+                    <Link href="/contacts">Contact List</Link>
+                    <Link href="/registers">User List</Link>
+
+                    {/* <Link
+                        href="/general-pcr/link2"
+                        className="p-2 hover:bg-gray-700"
+                    >
+                        2
+                    </Link> */}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SidebarMenu;
